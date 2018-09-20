05cf228246c3b1c58f88254d4cfcdf65817f9105
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-09-20 18:06:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,15 +51,7 @@ let color = '0xffffff'
 });
 
 
-const swearWords = ["كس", "طيز", "نيك", "سكس"]; 
-  if( swearWords.some(word => message.content.includes(word)) ) {
-    message.reply("مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ").then(sentMessage =>{
-      sentMessage.delete(20000)
-    })
-    message.delete(3000)
-    client.channels.get('489926688365477890').send(message.author.toString() + "استخدم كلام لا يليق ~")
-  }
-});
+
 
 
 
