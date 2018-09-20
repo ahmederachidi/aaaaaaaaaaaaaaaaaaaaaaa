ff25226570cc5bb999c1422cd09ee7e89cf73ed7
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-09-20 17:48:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,6 +51,21 @@ let color = '0xffffff'
 });
 
 
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 10) {
+        m.ban();
+    };
+});
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+};
+
+function datediff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+};
+
 //M3a4x
 var M3a4x = {};//M3a4x
 client.on('guildMemberRemove', member => {//M3a4x
