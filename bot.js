b4dd747f449959e4af88512e132cd53b75e9054d
ahmederachidi <42737175+ahmederachidi@users.noreply.github.com>
2018-11-01 13:20:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ let color = '0xffffff'
   client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('!bc')){
+if (message.content.startsWith('!leg')){
 if(!message.author.id === '434641589680209932') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
